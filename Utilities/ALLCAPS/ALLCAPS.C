5e4c9db437945f02eca72dcdb897794972c36de7
Carl Schelin <t.carl.schelin@gmail.com>
2021-09-15 00:02:03 +0000

    Utility to return the caps key to lower case

---
+++ Utilities/ALLCAPS/ALLCAPS.C
@@ -0,0 +1,19 @@
+/*
+
+	Programmer: Carl Schelin
+	Company: Hobgoblin Software
+	Utility: AllCaps
+	Description: This utility returns the caps state to lower case
+	no matter what the user requests. This is to prevent people who
+	like to use all caps for no reason from it's capabilities.
+	History: Carl Schelin	04/04/95	Initial Creation
+
+*/
+#include <windows.h>
+#include <toolhelp.h>
+
+/*
+	TASKENTRY FAR* lpte;
+
+	TaskFirst(lpte);
+
